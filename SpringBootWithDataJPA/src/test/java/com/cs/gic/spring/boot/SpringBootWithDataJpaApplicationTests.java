93e037bb1ea016ac186127daec3ca4e064459f02
cs <59759636+bchsekhar@users.noreply.github.com>
2022-02-15 15:21:22 +0530

    Add files via upload

---
+++ SpringBootWithDataJPA/src/test/java/com/cs/gic/spring/boot/SpringBootWithDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.gic.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
